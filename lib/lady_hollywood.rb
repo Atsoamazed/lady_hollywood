526156ec7385ecc3dbc23cfdd688b35781dafe20
Adri <at_soamazed@icloud.com>
2019-06-10 21:07:10 -0400

    updated require_relatives

--- lib/lady_hollywood.rb
+++ lib/lady_hollywood.rb
@@ -5,7 +5,9 @@ require 'pry'
 require_relative "./lady_hollywood/version"
 require_relative "./lady_hollwood/cli"
 require_relative "./lady_hollwood/lady-hollywood"
-
+require_relative "./lady_hollwood/scrapper"
+require_relative "./lady_hollwood/movies"
+require_relative "./lady_hollwood/shows"
 
 
 
