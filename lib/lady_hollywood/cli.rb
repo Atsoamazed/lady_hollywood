4c83cd24d0a5e2c168d483b167aa3cc9ae1cdf66
Adri <at_soamazed@icloud.com>
2019-06-12 22:15:13 -0400

    wip

--- lib/lady_hollywood/cli.rb
+++ lib/lady_hollywood/cli.rb
@@ -1,42 +1,52 @@
 # This is where all the cli user interaction is.
 class LadyHollywood::CLI
-
-def call
-    hollywood
-  #  movies
-  #  shows
+  puts "Welcome to Lady Hollywood Gem!"
+  def call
+    start
+    menu
+    list_ranking
     get_details
-end
+  end
 
-def hollywood
-    puts "Welcome to Lady Hollywood Gem! Would you like to view the top female characters in hollywood?[Y/N]"
-          answer = gets.strip
-    if answer == "n" || answer == "N"
-      puts "Sorry to hear that! Let's play another time. Bye bye!"
+  def start
+    puts "Would you like to view the top female characters in hollywood?[Y/N]"
+          answer = gets.strip.downcase
+    if answer == "n"
+      puts "Sorry to hear that. Let's play another time! Bye bye."
       exit
-      # add in a entry for invalid entry
-    else
+    end
+  end
+
+    def menu
        puts "Do you want the female ranking for movies or shows? Please enter [M/S]"
-     end
-            char = gets.strip
-    if char == "movie" || char == "MOVIE"|| char == "m" || char == "M"
+          char = gets.strip.downcase
+      if char == "movie" || char == "m"
           puts "Here is the list of top female characters in hollywood movies"
-           #list_movies return array of top female characters in movies
-    elsif char == "shows" || char == "SHOWS"|| char == "s" || char == "S"
+           list_movies #list_movies return array of top female characters in movies
+      elsif char == "shows" || char == "s"
           puts "Here is the list of top female characters in hollywood shows"
-           #list_shows return array of scapped data for shows
-    else
+          list_shows
+      else
           puts "Sorry, your input is invalid, Please try again."
-          return hollywood #need to add in a loop to reask the question
-end
+      return menu until char == "exit" #need to add in a loop to reask the question
+      end
+    end
 
-def get_details
-puts  "Enter the number of the ranking you'd like more info on"
-puts "Type list to see the the full list again"
-puts "Or type [exit] to exist:"
+=begin
+
+
+
+
+            def list_ranking
+            @ranking = LadyHollywood::Hollywood.ranks
+            @ranking.each.with_index(1) do |rank, i|
+            end
+            end
+=end
+  def get_details
+    puts  "Enter the number of the ranking you'd like more info on"
+    puts "Type list to see the the full list again"
+    puts "Or type [exit] to exist:"
     input = gets.strip
-
-end
-
-end
-end
+  end
+end #class ending
