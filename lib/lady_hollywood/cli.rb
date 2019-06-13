074c19b5d11c5a7424fa348d5bef86a12394488a
Adri <at_soamazed@icloud.com>
2019-06-12 23:56:04 -0400

    wip

--- lib/lady_hollywood/cli.rb
+++ lib/lady_hollywood/cli.rb
@@ -19,34 +19,36 @@ class LadyHollywood::CLI
 
     def menu
        puts "Do you want the female ranking for movies or shows? Please enter [M/S]"
+       #add in for invalid entry
           char = gets.strip.downcase
       if char == "movie" || char == "m"
           puts "Here is the list of top female characters in hollywood movies"
-           list_movies #list_movies return array of top female characters in movies
+          list_ranking #list_movies return array of top female characters in movies
       elsif char == "shows" || char == "s"
           puts "Here is the list of top female characters in hollywood shows"
-          list_shows
+        list_ranking
       else
           puts "Sorry, your input is invalid, Please try again."
-      return menu until char == "exit" #need to add in a loop to reask the question
+      return menu until char == "exit" #Exist does not work. needs to break if user types exit
       end
     end
 
-=begin
-
-
-
-
+#WIP
             def list_ranking
-            @ranking = LadyHollywood::Hollywood.ranks
-            @ranking.each.with_index(1) do |rank, i|
+              @ranking = LadyHollywood::Hollywood.ranks #list_movies return array of top female characters in movies
+               @ranking.each.with_index(1) do |rank, i, p, a|
+         puts "#{i}. #{rank.number} - #{rank.title} - #{rank.name}"
             end
             end
-=end
+#scrape needs order its just returning in chunks.
+# Also need to add to add in search by ranking, title, name option
+
   def get_details
     puts  "Enter the number of the ranking you'd like more info on"
     puts "Type list to see the the full list again"
     puts "Or type [exit] to exist:"
     input = gets.strip
   end
+
+  
 end #class ending
